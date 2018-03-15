d47e7464fe1132c8afc0aa709641dcb319218514
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-03-15 13:42:51 +0000

    automatically backed up by learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,6 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
+class Costume < ActiveRecord::Base
+  attr_accessor :name, :price, :size, :url
+
+end
--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,4 @@
 # Create your CostumeStore class here
+# class CostumeStore < ActiveRecord::Base
+#   attr_accessor :name, :location ,:number_inventory, :number_of_employees
+# end
